6371d223538f39bb4feb1cc9b7f6869075653129
Diederik <diederikdijkstra@hey.com>
2022-09-05 22:53:13 +0200

    ✨ Add extra functions

--- functions.php
+++ functions.php
@@ -170,6 +170,16 @@ require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
+ * Gravity Forms settings
+ */
+require get_template_directory() . '/inc/gravity-forms.php';
+
+/**
+ * Gutenberg Editor settings
+ */
+require get_template_directory() . '/inc/gutenberg-editor.php';
+
+/**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
