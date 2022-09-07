7875642639a782072b8b16e9a3a9c28fdcd3db7e
Diederik <diederikdijkstra@hey.com>
2022-09-07 09:25:24 +0200

    ✨ Update acf settings

--- inc/advanced-custom-fields.php
+++ inc/advanced-custom-fields.php
@@ -1,8 +1,64 @@
 <?php
 
+// ADD ACF OPTION PAGES
+if( function_exists('acf_add_options_page') ) {
+  acf_add_options_page(array(
+    'page_title' 	=> 'Thema Settings',
+    'menu_title'	=> 'Thema settings',
+    'menu_slug' 	=> 'theme-settings',
+    'capability'	=> 'edit_posts',
+    'redirect'		=> 'algemeen'
+  ));
+
+  acf_add_options_sub_page(array(
+    'page_title' 	=> 'Theme global settings',
+    'menu_title'	=> 'Algemeen',
+    'parent_slug'	=> 'theme-settings',
+  ));
+
+  acf_add_options_sub_page(array(
+    'page_title' 	=> 'Theme Header Settings',
+    'menu_title'	=> 'Header',
+    'parent_slug'	=> 'theme-settings',
+  ));
+
+  acf_add_options_sub_page(array(
+    'page_title' 	=> 'Theme Footer Settings',
+    'menu_title'	=> 'Footer',
+    'parent_slug'	=> 'theme-settings',
+  ));
+}
+
+// MOVE OPTION PAGES UP (UNDER DASHBOARD)
+function custom_menu_order( $menu_ord ) {
+
+  if (!$menu_ord) return true;
+
+  // vars
+  $menu = 'acf-options-algemeen';
+
+  // remove from current menu
+  $menu_ord = array_diff($menu_ord, array( $menu ));
+
+  // append after index.php [0]
+  array_splice( $menu_ord, 1, 0, array( $menu ) );
+
+  // Uncomment to view correct slug
+  // echo '<pre>';
+  // print_r( $menu_ord );
+  // echo '</pre>';
+  // die;
+
+  // return
+  return $menu_ord;
+}
+
+add_filter('custom_menu_order', 'custom_menu_order');
+add_filter('menu_order', 'custom_menu_order');
+
+// ADD TOOLBAR SELECT WITH FULL AND VERY SIMPLE OPTIONS
 add_filter( 'acf/fields/wysiwyg/toolbars' , 'my_toolbars'  );
-function my_toolbars( $toolbars )
-{
+function my_toolbars( $toolbars ){
   // Uncomment to view format of $toolbars
 
   // echo '< pre >';
