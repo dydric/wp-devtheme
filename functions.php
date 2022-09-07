e124248d4e542721f16c69893ad7a9bf623f5fe7
Diederik <diederikdijkstra@hey.com>
2022-09-07 09:26:10 +0200

    🔧 Disable unused default settings

--- functions.php
+++ functions.php
@@ -71,16 +71,16 @@ function wp_devtheme_setup() {
 	);
 
 	// Set up the WordPress core custom background feature.
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'wp_devtheme_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => '',
-			)
-		)
-	);
+	// add_theme_support(
+	// 	'custom-background',
+	// 	apply_filters(
+	// 		'wp_devtheme_custom_background_args',
+	// 		array(
+	// 			'default-color' => 'ffffff',
+	// 			'default-image' => '',
+	// 		)
+	// 	)
+	// );
 
 	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
@@ -90,15 +90,15 @@ function wp_devtheme_setup() {
 	 *
 	 * @link https://codex.wordpress.org/Theme_Logo
 	 */
-	add_theme_support(
-		'custom-logo',
-		array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		)
-	);
+	// add_theme_support(
+	// 	'custom-logo',
+	// 	array(
+	// 		'height'      => 250,
+	// 		'width'       => 250,
+	// 		'flex-width'  => true,
+	// 		'flex-height' => true,
+	// 	)
+	// );
 }
 add_action( 'after_setup_theme', 'wp_devtheme_setup' );
 
@@ -119,20 +119,20 @@ add_action( 'after_setup_theme', 'wp_devtheme_content_width', 0 );
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function wp_devtheme_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'wp-devtheme' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'wp-devtheme' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'wp_devtheme_widgets_init' );
+// function wp_devtheme_widgets_init() {
+// 	register_sidebar(
+// 		array(
+// 			'name'          => esc_html__( 'Sidebar', 'wp-devtheme' ),
+// 			'id'            => 'sidebar-1',
+// 			'description'   => esc_html__( 'Add widgets here.', 'wp-devtheme' ),
+// 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+// 			'after_widget'  => '</section>',
+// 			'before_title'  => '<h2 class="widget-title">',
+// 			'after_title'   => '</h2>',
+// 		)
+// 	);
+// }
+// add_action( 'widgets_init', 'wp_devtheme_widgets_init' );
 
 /**
  * Enqueue scripts and styles.
@@ -152,7 +152,7 @@ add_action( 'wp_enqueue_scripts', 'wp_devtheme_scripts' );
 /**
  * Implement the Custom Header feature.
  */
-require get_template_directory() . '/inc/custom-header.php';
+// require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
